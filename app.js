38fd4433fa8d201791952f5caaf07a2f183a5ec7
Raminta Alisauskaite <raminta.alisauskaite@gmail.com>
2023-11-15 20:36:30 +0200

    Revert "vercel.json file added, app.js changed to index.js"
    This reverts commit d9fc21c1a24fc0862dd649d6f97c3501d37eb792.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const index = express();
+const app = express();
 const mongoose = require('mongoose');
 const port = 8000;
 const router = require('./routers/mainRouters');
 const {createServer} = require('node:http');
-const server = createServer(index);
+const server = createServer(app);
 require('./modules/sockets')(server);
 require('dotenv').config();
 
@@ -16,9 +16,9 @@ mongoose.connect(process.env.DB_KEY)
     console.log('error while connecting to DB', e)
 })
 
-index.use(cors());
-index.use(express.json());
-index.use('/', router)
+app.use(cors());
+app.use(express.json());
+app.use('/', router)
 
 server.listen(port, () => {
     console.log('server running on localhost:'+port);
